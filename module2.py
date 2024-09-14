0e87c057b30b74ac4103ac1aaa60782e7f506c48
Agnieszka Malek <aga19m77@gmail.com>
2024-09-14 17:28:45 +0100

    Initial commit

---
+++ module2.py
@@ -0,0 +1,15 @@
+# Ice Cream Price Checker
+
+# Define Ice cream prices
+Vanilla_ice_cream = 25
+Chocolate_ice_cream = 37
+
+# Check which Ice cream is less expensive
+if Vanilla_ice_cream < Chocolate_ice_cream:
+    print("Vanilla Ice cream is less expensive than Chocolate Ice cream.")
+elif Chocolate_ice_cream < Vanilla_ice_cream:
+    print("Chocolate Ice cream is less expensive than Vanilla Ice cream.")
+else:
+    print("Both products have the same price.")
+
+
