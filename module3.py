65af2ea38b8c074c34bef92e6d4028f869871cc2
Agnieszka Malek <aga19m77@gmail.com>
2024-09-14 18:51:00 +0100

    Activity 3 - completed.

---
+++ module3.py
@@ -0,0 +1,12 @@
+# Personal Information Card
+name = "Agnieszka"
+age = 47
+city = "Haddington"
+hobby = "Pyrography, especially during the winter time and swimming all year round"
+
+# Expose to view personal information
+print("PERSONAL INFORMATION")
+print("Name: ", name)
+print("Age: ", age)
+print("City: ", city)
+print("Hobby: ", hobby)
